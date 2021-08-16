e3a256c0f720b1939a456d5493884986dab6f602
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2021-08-16 04:01:38 +0100

    correct-build

--- src/components/skeleton/index.jsx
+++ src/components/skeleton/index.jsx
@@ -1,16 +1,16 @@
-import Skeleton, {SkeletonTheme} from 'react-loading-skeleton';
+// import Skeleton, {SkeletonTheme} from 'react-loading-skeleton';
 
-const LoaderInterface = () => {
-  return (
-    <div className="product">
-      <div className="product__description">
-        <Skeleton height={170} width={100} className="product__description__img" />
-        <h5><Skeleton count={5} /></h5>
-      </div>
-      <p className="product__price"><Skeleton count={5} /></p>
-      <Skeleton className="product__btn" count={5} />
-    </div>
-  )
-}
+// const LoaderInterface = () => {
+//   return (
+//     <div className="product">
+//       <div className="product__description">
+//         <Skeleton height={170} width={100} className="product__description__img" />
+//         <h5><Skeleton count={5} /></h5>
+//       </div>
+//       <p className="product__price"><Skeleton count={5} /></p>
+//       <Skeleton className="product__btn" count={5} />
+//     </div>
+//   )
+// }
 
-export default LoaderInterface;
+// export default LoaderInterface;
